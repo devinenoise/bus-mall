b57f88b1273816c060897a221a401d59ac1302cc
Kyle Devine <devinenoise@gmail.com>
2020-01-28 10:55:09 -0800

    fixing local repo

---
+++ app.js
@@ -0,0 +1,5 @@
+import { products } from './products.js';
+
+const productImage = document.getElementById('img');
+const productRadioTag = document.getElementById('input');
+const productName = document.getElementById('name');
