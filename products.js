154584a87e4df03bb4757b0b39b6011c1e281b2e
Kyle Devine <devinenoise@gmail.com>
2020-01-28 10:39:32 -0800

    first commit

---
+++ products.js
@@ -0,0 +1,102 @@
+export const products = [
+    {
+        id: space-bag,
+        name: 'Space Bag',
+        image: 'assets/bag.jpg'
+    },
+    {
+        id: banana-slicer,
+        name: 'Banana Slicer',
+        image: 'assets/banana.jpg'
+    },
+    {
+        id: bathroom,
+        name: 'Bathroom Reader Stand',
+        image: 'assets/bathroom.jpg'
+    },
+    {
+        id: boots,
+        name: 'Rain Boots',
+        image: 'assets/boots.jpg'
+    },
+    {
+        id: breakfast,
+        name: 'Breakfast Maker',
+        image: 'assets/breakfast.jpg'
+    },
+    {
+        id: bubblegum,
+        name: 'Meatball Gum',
+        image: 'assets/bubblegum.jpg'
+    },
+    {
+        id: chair,
+        name: 'Chair',
+        image: 'assets/chair.jpg'
+    },
+    {
+        id: cthulhu,
+        name: 'Cthulhu Action Figure',
+        image: 'assets/cthulhu.jpg'
+    },
+    {
+        id: dog-duck,
+        name: 'Duck Mask For Dogs',
+        image: 'assets/dog-duck.jpg'
+    },
+    {
+        id: dragon,
+        name: 'Dragon Meat',
+        image: 'assets/dragon.jpg'
+    },
+    {
+        id: pen,
+        name: 'Pen Cutlery',
+        image: 'assets/pen.jpg'
+    },
+    {
+        id: pet-sweep,
+        name: 'Pet Sweep',
+        image: 'assets/pet-sweep.jpg'
+    },
+    {
+        id: scissors,
+        name: 'Pizza Scissors',
+        image: 'assets/scissors.jpg'
+    },
+    {
+        id: shark,
+        name: 'Shark Sleeping Bag',
+        image: 'assets/shark.jpg'
+    },
+    {
+        id: sweep,
+        name: 'Pet Sweep',
+        image: 'assets/pet-sweep.jpg'
+    },
+    {
+        id: tauntaun,
+        name: 'Tauntaun Sleeping Bag',
+        image: 'assets/tauntaun.jpg'
+    },
+    {
+        id: unicorn,
+        name: 'Unicorn Meat',
+        image: 'assets/unicorn.jpg'
+    },
+    {
+        id: usb,
+        name: 'USB Tentacle',
+        image: 'assets/usb.gif'
+    },
+    {
+        id: water-can,
+        name: 'Water Can',
+        image: 'assets/water-can.jpg'
+    },
+    {
+        id: wine-glass,
+        name: 'Wine Glass',
+        image: 'assets/wine-glass.jpg'
+    },
+];
